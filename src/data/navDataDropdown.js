30942d397a175c4deb5d1a941af693c5b777c6e0
CapnBiggin <capnbiggin@proton.me>
2025-06-22 16:49:10 -0500

    NavBar

---
+++ src/data/navDataDropdown.js
@@ -0,0 +1,20 @@
+const navDropdown = [
+	{
+		name: "TCG Player Store",
+		path: "https://www.tcgplayer.com/search/all/product?seller=7d154405&view=grid",
+	},
+	{
+		name: "Facebook",
+		path: "https://www.facebook.com/gamersguildal/",
+	},
+	{
+		name: "Tatto Facebook",
+		path: "https://www.facebook.com/theguildALtattoostudio",
+	},
+	{
+		name: "Discord",
+		path: "https://discord.gg/vXDwxXAVqf",
+	},
+];
+
+export default navDropdown;
---
+++ src/data/navDataTournamentDropdown.js
@@ -0,0 +1,16 @@
+const navTournamentDropdown = [
+	{
+		name: "Flesh and Blood",
+		path: "https://fabtcg.com/en/locator/gamers-guild-al/",
+	},
+	{
+		name: "Magic the Gathering",
+		path: "https://locator.wizards.com/store/15234",
+	},
+	{
+		name: "TopDeck",
+		path: "https://topdeck.gg/hubs/K2s47dM7wtadge2uzKvM286HQyo1",
+	},
+];
+
+export default navTournamentDropdown;
