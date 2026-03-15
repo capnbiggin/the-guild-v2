c69c3fae351fefe8aa7c9177bfe272202ae085ee
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-03-15 17:54:57 -0500

    Added links to timer and spinner - Sun Mar 15 05:54:57 PM CDT 2026

--- src/data/navDataDropdown.js
+++ src/data/navDataDropdown.js
@@ -1,16 +1,26 @@
 const navDropdown = [
-	{
-		name: "TCG Player Store",
-		path: "https://www.tcgplayer.com/search/all/product?seller=7d154405&view=grid",
-	},
-	{
-		name: "Facebook",
-		path: "https://www.facebook.com/gamersguildal",
-	},
-	{
-		name: "Discord",
-		path: "https://discord.gg/vXDwxXAVqf",
-	},
+  {
+    name: "TCG Player Store",
+    path: "https://www.tcgplayer.com/search/all/product?seller=7d154405&view=grid",
+  },
+  {
+    name: "Facebook",
+    path: "https://www.facebook.com/gamersguildal",
+  },
+  {
+    name: "Discord",
+    path: "https://discord.gg/vXDwxXAVqf",
+  },
+  ,
+  {
+    name: "Guild Game Timer",
+    path: "https://timer.theguildal.com",
+  },
+  ,
+  {
+    name: "Guild Spinner",
+    path: "https://spinner.theguildal.com",
+  },
 ];
 
 export default navDropdown;
